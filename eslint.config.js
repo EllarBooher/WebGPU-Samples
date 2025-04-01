379e0611edd6194d95272f97d57188bdc863bc8f
Estelle Booher <15761263+EllarBooher@users.noreply.github.com>
2025-03-31 17:06:04 -0700

    add missing jsxA11y eslint plugin

--- eslint.config.js
+++ eslint.config.js
@@ -5,6 +5,7 @@ import reactRefresh from "eslint-plugin-react-refresh";
 import tsdoc from "eslint-plugin-tsdoc";
 import tseslint from "typescript-eslint";
 import react from "eslint-plugin-react";
+import jsxA11y from "eslint-plugin-jsx-a11y";
 
 export default tseslint.config(
 	{ ignores: ["dist"] },
