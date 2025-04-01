ca902a81f92bc59194eb8119f73f5560843470c2
Estelle Booher <15761263+EllarBooher@users.noreply.github.com>
2025-04-01 13:38:25 -0700

    update whitespace

--- src/webgpu/RendererApp.ts
+++ src/webgpu/RendererApp.ts
@@ -6,22 +6,22 @@ import { GUI } from "lil-gui";
  * backend (just lil-gui for now).
  */
 export interface RendererApp {
-  quit: boolean;
-  presentationInterface(): GPUCanvasConfiguration;
-  setLowPerformanceMode?: (isLowPerf: boolean) => void;
-  /*
-   * TODO: We could maybe just resize resources on draw if the presentTexture
-   * is an unexpected size, cutting this handleResize method.
-   */
-  handleResize?: (newWidth: number, newHeight: number) => void;
-  draw: (
-    presentTexture: GPUTexture,
-    aspectRatio: number,
-    timeMilliseconds: number,
-    deltaTimeMilliseconds: number
-  ) => void;
-  setupUI?: (gui: GUI) => void;
-  destroy(): void;
+	quit: boolean;
+	presentationInterface(): GPUCanvasConfiguration;
+	setLowPerformanceMode?: (isLowPerf: boolean) => void;
+	/*
+	 * TODO: We could maybe just resize resources on draw if the presentTexture
+	 * is an unexpected size, cutting this handleResize method.
+	 */
+	handleResize?: (newWidth: number, newHeight: number) => void;
+	draw: (
+		presentTexture: GPUTexture,
+		aspectRatio: number,
+		timeMilliseconds: number,
+		deltaTimeMilliseconds: number
+	) => void;
+	setupUI?: (gui: GUI) => void;
+	destroy(): void;
 }
 
 /**
@@ -36,8 +36,8 @@ export interface RendererApp {
  * draws
  */
 export type RendererAppConstructor = (
-  device: GPUDevice,
-  canvasFormat: GPUTextureFormat
+	device: GPUDevice,
+	canvasFormat: GPUTextureFormat
 ) => RendererApp;
 
 /**
@@ -49,108 +49,111 @@ export type RendererAppConstructor = (
  * unsupported but required features.
  */
 async function getDevice(props: {
-  gpu: GPU;
-  requiredFeatures: ReadonlySet<GPUFeatureName>;
-  optionalFeatures: ReadonlySet<GPUFeatureName>;
-  requiredLimits: ReadonlyMap<keyof GPUSupportedLimits, number>;
+	gpu: GPU;
+	requiredFeatures: ReadonlySet<GPUFeatureName>;
+	optionalFeatures: ReadonlySet<GPUFeatureName>;
+	requiredLimits: ReadonlyMap<keyof GPUSupportedLimits, number>;
 }): Promise<{ adapter: GPUAdapter; device: GPUDevice }> {
-  console.log("Starting WebGPU");
+	console.log("Starting WebGPU");
 
-  const adapterPromise = navigator.gpu
-    .requestAdapter()
-    .then((value) => {
-      if (!value) {
-        return Promise.reject(
-          new Error("Requested WebGPU Adapter is not available.")
-        );
-      }
-      return Promise.resolve(value);
-    })
-    .catch((reason) => {
-      return Promise.reject(
-        new Error("Unable to get WebGPU Adapter", { cause: reason })
-      );
-    });
+	const adapterPromise = navigator.gpu
+		.requestAdapter()
+		.then((value) => {
+			if (!value) {
+				return Promise.reject(
+					new Error("Requested WebGPU Adapter is not available.")
+				);
+			}
+			return Promise.resolve(value);
+		})
+		.catch((reason) => {
+			return Promise.reject(
+				new Error("Unable to get WebGPU Adapter", { cause: reason })
+			);
+		});
 
-  const devicePromise = adapterPromise.then((adapter) => {
-    const knownRequiredFeatures = Array.from(
-      props.requiredFeatures.values()
-    ).filter((feature) => {
-      return adapter.features.has(feature);
-    });
-    if (knownRequiredFeatures.length != props.requiredFeatures.size) {
-      const reason = `Required features unavailable: ${Array.from(
-        props.requiredFeatures.values()
-      )
-        .filter((feature) => !adapter.features.has(feature))
-        .map((feature) => `'${feature}'`)
-        .join(",")}`;
-      return Promise.reject(
-        new Error("Unable to get WebGPU Device", { cause: reason })
-      );
-    }
-    const features = knownRequiredFeatures.concat(
-      ...Array.from(props.optionalFeatures.values()).filter((feature) => {
-        return adapter.features.has(feature);
-      })
-    );
-    console.log(`Enabling features: '${features.join(`', '`)}'`);
+	const devicePromise = adapterPromise.then((adapter) => {
+		const knownRequiredFeatures = Array.from(
+			props.requiredFeatures.values()
+		).filter((feature) => {
+			return adapter.features.has(feature);
+		});
+		if (knownRequiredFeatures.length != props.requiredFeatures.size) {
+			const reason = `Required features unavailable: ${Array.from(
+				props.requiredFeatures.values()
+			)
+				.filter((feature) => !adapter.features.has(feature))
+				.map((feature) => `'${feature}'`)
+				.join(",")}`;
+			return Promise.reject(
+				new Error("Unable to get WebGPU Device", { cause: reason })
+			);
+		}
+		const features = knownRequiredFeatures.concat(
+			...Array.from(props.optionalFeatures.values()).filter((feature) => {
+				return adapter.features.has(feature);
+			})
+		);
+		console.log(`Enabling features: '${features.join(`', '`)}'`);
 
-    const satisfiedRequiredLimits = new Map<keyof GPUSupportedLimits, number>();
-    const missingLimits = new Array<{
-      name: keyof GPUSupportedLimits;
-      requestedMinimum: number;
-      supported: number;
-    }>();
-    for (const [name, requestedMinimum] of props.requiredLimits.entries()) {
-      const supported = adapter.limits[name] as number;
+		const satisfiedRequiredLimits = new Map<
+			keyof GPUSupportedLimits,
+			number
+		>();
+		const missingLimits = new Array<{
+			name: keyof GPUSupportedLimits;
+			requestedMinimum: number;
+			supported: number;
+		}>();
+		for (const [name, requestedMinimum] of props.requiredLimits.entries()) {
+			const supported = adapter.limits[name] as number;
 
-      if (supported >= requestedMinimum) {
-        satisfiedRequiredLimits.set(name, requestedMinimum);
-      } else {
-        missingLimits.push({
-          name: name,
-          requestedMinimum: requestedMinimum,
-          supported: supported,
-        });
-      }
-    }
-    if (satisfiedRequiredLimits.size < props.requiredLimits.size) {
-      const reason = `Required limits unsatisfied: ${missingLimits
-        .map(
-          (limit) =>
-            `( name: '${limit.name}' supported: '${limit.supported}' requested: '${limit.requestedMinimum}' )`
-        )
-        .join(",")}`;
-      return Promise.reject(
-        new Error("Unable to get WebGPU Device", { cause: reason })
-      );
-    }
+			if (supported >= requestedMinimum) {
+				satisfiedRequiredLimits.set(name, requestedMinimum);
+			} else {
+				missingLimits.push({
+					name: name,
+					requestedMinimum: requestedMinimum,
+					supported: supported,
+				});
+			}
+		}
+		if (satisfiedRequiredLimits.size < props.requiredLimits.size) {
+			const reason = `Required limits unsatisfied: ${missingLimits
+				.map(
+					(limit) =>
+						`( name: '${limit.name}' supported: '${limit.supported}' requested: '${limit.requestedMinimum}' )`
+				)
+				.join(",")}`;
+			return Promise.reject(
+				new Error("Unable to get WebGPU Device", { cause: reason })
+			);
+		}
 
-    const limits: Record<string, number> = {};
-    for (const [name, limit] of satisfiedRequiredLimits) {
-      limits[name] = limit;
-    }
+		const limits: Record<string, number> = {};
+		for (const [name, limit] of satisfiedRequiredLimits) {
+			limits[name] = limit;
+		}
 
-    return adapter
-      .requestDevice({
-        requiredFeatures: features,
-        requiredLimits: limits,
-      })
-      .catch((reason) => {
-        return Promise.reject(
-          new Error("Unable to get WebGPU Device", { cause: reason })
-        );
-      });
-  });
+		return adapter
+			.requestDevice({
+				requiredFeatures: features,
+				requiredLimits: limits,
+			})
+			.catch((reason) => {
+				return Promise.reject(
+					new Error("Unable to get WebGPU Device", { cause: reason })
+				);
+			});
+	});
 
-  return Promise.all([adapterPromise, devicePromise]).then((value) => {
-    const [adapter, device] = value;
-    return {
-      adapter,
-      device,
-    };
-  });
+	return Promise.all([adapterPromise, devicePromise]).then((value) => {
+		const [adapter, device] = value;
+		return {
+			adapter,
+			device,
+		};
+	});
 }
 
 /**
@@ -172,43 +175,43 @@ async function getDevice(props: {
  * @returns A promise resolving to the fully initialized app. Can reject.
  */
 export async function initializeApp(props: {
-  gpu: GPU;
-  requiredLimits: ReadonlyMap<keyof GPUSupportedLimits, number>;
-  requiredFeatures: ReadonlySet<GPUFeatureName>;
-  optionalFeatures: ReadonlySet<GPUFeatureName>;
-  import: () => Promise<RendererAppConstructor>;
-  onUncapturedError: (e: GPUUncapturedErrorEvent) => void;
+	gpu: GPU;
+	requiredLimits: ReadonlyMap<keyof GPUSupportedLimits, number>;
+	requiredFeatures: ReadonlySet<GPUFeatureName>;
+	optionalFeatures: ReadonlySet<GPUFeatureName>;
+	import: () => Promise<RendererAppConstructor>;
+	onUncapturedError: (e: GPUUncapturedErrorEvent) => void;
 }): Promise<RendererApp> {
-  return Promise.all([
-    props.import(),
-    getDevice({
-      ...props,
-    }),
-  ]).then(([sampleConstructor, { adapter: _adapter, device }]) => {
-    const canvasFormat = props.gpu.getPreferredCanvasFormat();
-    const app = sampleConstructor(device, canvasFormat);
+	return Promise.all([
+		props.import(),
+		getDevice({
+			...props,
+		}),
+	]).then(([sampleConstructor, { adapter: _adapter, device }]) => {
+		const canvasFormat = props.gpu.getPreferredCanvasFormat();
+		const app = sampleConstructor(device, canvasFormat);
 
-    device.lost
-      .then(
-        (reason) => {
-          console.log(
-            `WebGPU device lost - ("${reason.reason}"):\n ${reason.message}`
-          );
-        },
-        (err) => {
-          // This shouldn't happen
-          throw new Error(`WebGPU device lost rejected`, {
-            cause: err,
-          });
-        }
-      )
-      .finally(() => {
-        app.quit = true;
-      });
-    device.onuncapturederror = (ev): void => {
-      app.quit = true;
-      props.onUncapturedError(ev);
-    };
-    return app;
-  });
+		device.lost
+			.then(
+				(reason) => {
+					console.log(
+						`WebGPU device lost - ("${reason.reason}"):\n ${reason.message}`
+					);
+				},
+				(err) => {
+					// This shouldn't happen
+					throw new Error(`WebGPU device lost rejected`, {
+						cause: err,
+					});
+				}
+			)
+			.finally(() => {
+				app.quit = true;
+			});
+		device.onuncapturederror = (ev): void => {
+			app.quit = true;
+			props.onUncapturedError(ev);
+		};
+		return app;
+	});
 }
